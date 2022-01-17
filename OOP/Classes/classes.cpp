9ceee9f832d852031034fa5f37a8f05e03ba4469
Muhammad Sami Shoukat <88966319+samishoukat12@users.noreply.github.com>
2022-01-18 00:25:36 +0500

    classes

---
+++ OOP/Classes/classes.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+
+class FirstClass{
+    private:
+    int n;
+    public:
+    void input(){
+        cout<<"enter value of n"<<":";
+        cin>>n;
+    }
+     void output(){
+         cout<<n;
+     }
+};
+ int main(){
+     FirstClass obj1;
+     obj1.input();
+     obj1.output();
+     getch();
+ }
